85928fd3ff601e7dbf3c1efc9fa3a66fe50b9723
zhangguangcen <zhangguangcen@inke.cn>
2021-02-08 11:39:06 +0800

    RENAME project: Practice -> PracticeFramework

---
+++ PracticeFramework/PracticeFramework/PracticeFramework.h
@@ -0,0 +1,18 @@
+//
+//  PracticeFramework.h
+//  PracticeFramework
+//
+//  Created by zgc_inke on 2021/2/8.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PracticeFramework.
+FOUNDATION_EXPORT double PracticeFrameworkVersionNumber;
+
+//! Project version string for PracticeFramework.
+FOUNDATION_EXPORT const unsigned char PracticeFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PracticeFramework/PublicHeader.h>
+
+
